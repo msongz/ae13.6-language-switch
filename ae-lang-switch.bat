2774256cd90eb7598d24e1d1edbd6600d2eb9f2b
songZ <msongz@qq.com>
2016-10-10 13:50:16 +0800

    Add files via upload

---
+++ ae-lang-switch.bat
@@ -0,0 +1,11 @@
+@echo off
+
+
+
+set aeF=C:\Users\%USERNAME%\Documents
+
+if exist "%aeF%\ae_force_english.txt" (ren "%aeF%\ae_force_english.txt" "#ae_force_english.txt") else (ren "%aeF%\#ae_force_english.txt" ae_force_english.txt)
+
+if exist "%aeF%\ae_force_english.txt" (echo 现在 ae 是英文版) else (echo 现在 ae 是中文版)
+
+pause
